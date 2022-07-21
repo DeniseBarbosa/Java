56282139351835c05e173336f1004407f7ef96bb
DeniseBarbosa <deniseluvoir@gmail.com>
2022-07-21 15:44:27 -0300

    Hello World

---
+++ Hello/src/App.java
@@ -0,0 +1,11 @@
+
+/**
+ Hello World
+ Denise Pereira 
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
